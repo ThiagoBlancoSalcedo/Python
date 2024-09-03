e5c840e278393a2293ccc0e5fd215bdb0a784c5d
ThiagoBlancoSalcedo <202410473@urp.edu.pe>
2024-09-03 18:51:27 +0000

    Hola

---
+++ Clase_auto.py
@@ -0,0 +1,27 @@
+# Simila el funcionamiento en un auto
+class Auto:
+    # Función para definir el constructor
+    def __init__(self,marca,disp_gasolina):
+        self.marca= marca
+        self.disp_gasolina = disp_gasolina
+    # Definir los estados
+    def avanzar(self, km):
+        self.disp_gasolina -= km / 40
+    def retroceder (self, km):
+        self.disp_gasolina -= km / 20
+# Crear objetos
+auto1 = Auto('Toyota',15)
+auto2 = Auto('Ford',10)
+
+print('Auto1: '+ auto1.marca + ', Gas ' + str(auto1.disp_gasolina))
+print('Auto2: '+ auto2.marca + ', Gas ' + str(auto2.disp_gasolina))
+
+# Recorrer los autos
+auto1.avanzar(120)
+auto1.retroceder(10)
+
+auto2.avanzar(160)
+auto2.retroceder(40)
+
+print('Auto1:'+ auto1.marca + ', Gas ' + str(auto1.disp_gasolina))
+print('Auto2:'+ auto2.marca + ', Gas ' + str(auto2.disp_gasolina))
